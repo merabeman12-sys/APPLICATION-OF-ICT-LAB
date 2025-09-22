277584e569acc02d3bbb31e8cb03f9c5f6a5125d
MERAB EMAN <merabeman12@gmail.com>
2025-09-22 13:38:18 +0500

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,11 @@
+
+
+a = 10
+b = 3
+print("addition",a+b)
+print("difference",a-b)
+print("product",a*b)
+print("division",a/b)
+print("float division",a//b)
+print("modulus",a%b)
+print("exponent",a**b)
---
+++ task 2.py
@@ -0,0 +1,10 @@
+
+
+name = "Merab Eman"
+age = 13
+height = 5.2
+is_student= True
+print(name)
+print(age)
+print (height)
+print(is_student)
